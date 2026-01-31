014f68f3856856630d3c876e961fd4fcc933f45e
Muhammad Faraz <mfarazzzz@gmail.com>
2026-01-31 21:20:25 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { getCMSProvider } from "@/services/cms";
 
+export const dynamic = 'force-dynamic';
+
 const BASE_URL = "https://rampurnews.com";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {

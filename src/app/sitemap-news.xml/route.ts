dde4eb5e5b87f375ae02ce219f8df672131420e6
Muhammad Faraz <mfarazzzz@gmail.com>
2026-02-23 18:50:11 +0530

    Delete route.ts

--- src/app/sitemap-news.xml/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const dynamic = "force-dynamic";
-
-export async function GET(request: Request) {
-  return NextResponse.redirect(new URL("/news-sitemap.xml", request.url), 301);
-}
